8c567c622bf7675113f05d8cc2d54af67a8f3b6c
Satuki717 <41312275+Satuki717@users.noreply.github.com>
2021-05-31 21:59:01 +0800

    Update cuda+mpi.cu

--- 16_final_report/cuda+mpi.cu
+++ 16_final_report/cuda+mpi.cu
@@ -19,6 +19,7 @@ __global__ void matmul(float *A, float *B, float *C, int N){
 }
 
 int main(int argc, char** argv) {
+
   int size, rank;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -27,13 +28,7 @@ int main(int argc, char** argv) {
   const int N = 2048;
   const int M = 1024;
   int gpusize = N * N * sizeof(float);
-  //vector<float> A(N*N);
-  //vector<float> B(N*N);
-  //vector<float> C(N*N, 0);
-  vector<float> subA(N*N/size);
-  vector<float> subB(N*N/size);
-  vector<float> subC(N*N/size, 0);
-  vector<float> recv(N*N/size);
+
   float *A, *B, *C;
   cudaMallocManaged(&A, gpusize);
   cudaMallocManaged(&B, gpusize);
@@ -46,15 +41,6 @@ int main(int argc, char** argv) {
     }
   }
   dim3 grid(N/M, N);
-  int offset = N/size*rank;
-  for (int i=0; i<N/size; i++)
-    for (int j=0; j<N; j++)
-      subA[N*i+j] = A[N*(i+offset)+j];
-  for (int i=0; i<N; i++)
-    for (int j=0; j<N/size; j++)
-      subB[N/size*i+j] = B[N*i+j+offset];
-  int recv_from = (rank + 1) % size;
-  int send_to = (rank - 1 + size) % size;
 
   double comp_time = 0, comm_time = 0;
 
@@ -62,21 +48,12 @@ int main(int argc, char** argv) {
     auto tic = chrono::steady_clock::now();
     matmul<<<grid, M>>>(A,B,C,N);
     cudaDeviceSynchronize();
-    // offset = N/size*((rank+irank) % size);
-    // for (int i=0; i<N/size; i++)
-    //   for (int j=0; j<N/size; j++)
-    //     for (int k=0; k<N; k++)
-    //       subC[N*i+j+offset] += subA[N*i+k] * subB[N/size*k+j];
     auto toc = chrono::steady_clock::now();
     comp_time += chrono::duration<double>(toc - tic).count();
-    MPI_Request request[2];
-    MPI_Isend(&subB[0], N*N/size, MPI_FLOAT, send_to, 0, MPI_COMM_WORLD, &request[0]);
-    MPI_Irecv(&recv[0], N*N/size, MPI_FLOAT, recv_from, 0, MPI_COMM_WORLD, &request[1]);
-    MPI_Waitall(2, request, MPI_STATUS_IGNORE);
     tic = chrono::steady_clock::now();
     comm_time += chrono::duration<double>(tic - toc).count();
   }
-  MPI_Allgather(&subC[0], N*N/size, MPI_FLOAT, &C[0], N*N/size, MPI_FLOAT, MPI_COMM_WORLD);
+  
 
   for (int i=0; i<N; i++)
     for (int j=0; j<N; j++)
